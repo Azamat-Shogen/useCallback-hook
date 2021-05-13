00f72c965ed44a88d2494f0511dd6c7574902c7e
Azamat Shogen <68517175+Azamat-Shogen@users.noreply.github.com>
2021-05-13 15:38:35 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+import ParentComponent from './components/ParentComponent';
+
+
+export default function App() {
+  
+  return (
+    <div>
+     <ParentComponent />
+    </div>
+  );
+}
